5d0f9fab32bcd567d6d4d7736498d3d2ab445e35
Michal <voj38077@spseol.cz>
2022-04-04 09:34:27 +0200

    skola

--- kalkulacka.py
+++ kalkulacka.py
@@ -4,39 +4,12 @@
 # Autor:   Marek Nožka, nozka <@t> spseol <d.t> cz
 ############################################################################
 import math
+from os.path import basename, splitext
+import tkinter as tk
+
+# from tkinter import ttk
 
 zasobnik = []
-
-
-def operace(token):
-    if token.upper() == "Q":
-        exit()
-    if token.upper() == "PI":
-        zasobnik.append(math.pi)
-    if token == "+":
-        b = zasobnik.pop()
-        a = zasobnik.pop()
-        zasobnik.append(a + b)
-    if token == "-":
-        b = zasobnik.pop()
-        a = zasobnik.pop()
-        zasobnik.append(a - b)
-    if token == "*":
-        b = zasobnik.pop()
-        a = zasobnik.pop()
-        zasobnik.append(a * b)
-    if token == "**":
-        b = zasobnik.pop()
-        a = zasobnik.pop()
-        zasobnik.append(a ** b)
-    if token == "sin":
-        a = zasobnik.pop()
-        zasobnik.append(math.sin(a))
-    if token == "cos":
-        a = zasobnik.pop()
-        zasobnik.append(math.cos(a))
-
-
 dva_operandy = {}
 dva_operandy["+"] = lambda a, b: a + b
 dva_operandy["-"] = lambda a, b: a - b
@@ -52,41 +25,75 @@ jeden_operand["tg"] = math.tan
 jeden_operand["tan"] = math.tan
 
 
-def operace(token):
-    if token.upper() == "Q":
-        exit()
-    if token.upper() == "PI":
-        zasobnik.append(math.pi)
-    if token.upper() == "SW":
-        b = zasobnik.pop()
-        a = zasobnik.pop()
-        zasobnik.append(b)
-        zasobnik.append(a)
-    if token in dva_operandy.keys():
-        if len(zasobnik) >= 2:
+class About(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent, class_=parent.name)
+        self.config()
+
+        btn = tk.Button(self, text="Konec", command=self.close)
+        btn.pack()
+
+    def close(self):
+        self.destroy()
+
+
+class Application(tk.Tk):
+    name = basename(splitext(basename(__file__.capitalize()))[0])
+    name = "Kalkulkačka"
+
+    def __init__(self):
+
+        super().__init__(className=self.name)
+
+        self.var_entry = tk.IntVar()
+        self.title(self.name)
+        self.bind("<Escape>", self.quit)
+        self.lbl = tk.Label(self, text="Kalkulačka")
+        self.lbl.pack()
+        self.entry = tk.Entry(self, textvariable= self.var_entry)
+        self.entry.pack()
+        self.btn2 = tk.Button(self, text="Zapsat hodnotu", command=self.zpracuj)
+        self.btn2.pack()
+        self.btn = tk.Button(self, text="Quit", command=self.quit)
+        self.btn.pack()
+
+    def quit(self, event=None):
+        super().quit()
+
+
+    def operace(self, token):
+        if token.upper() == "Q":
+            exit()
+        if token.upper() == "PI":
+            zasobnik.append(math.pi)
+        if token.upper() == "SW":
             b = zasobnik.pop()
             a = zasobnik.pop()
-            zasobnik.append(dva_operandy[token](a, b))
-        else:
-            print("Nemám dost operandů!!!")
-    if token in jeden_operand.keys():
-        if len(zasobnik) >= 1:
-            a = zasobnik.pop()
-            zasobnik.append(jeden_operand[token](a))
-        else:
-            print("Nemám dost operandů!!!")
+            zasobnik.append(b)
+            zasobnik.append(a)
+        if token in dva_operandy.keys():
+            if len(zasobnik) >= 2:
+                b = zasobnik.pop()
+                a = zasobnik.pop()
+                zasobnik.append(dva_operandy[token](a, b))
+            else:
+                print("Nemám dost operandů!!!")
+        if token in jeden_operand.keys():
+            if len(zasobnik) >= 1:
+                a = zasobnik.pop()
+                zasobnik.append(jeden_operand[token](a))
+            else:
+                print("Nemám dost operandů!!!")
 
 
-def zpracuj(radek):
-    tokeny = radek.split()
-    for token in tokeny:
+    def zpracuj(self):
+        token = self.var_entry.get()
         try:
             zasobnik.append(float(token))
         except ValueError:
-            operace(token)
+            operace(self,token)
 
 
-# čtu vstup
-while True:
-    radek = input(zasobnik.__repr__() + ">>> ")
-    zpracuj(radek)
+app = Application()
+app.mainloop()
+
